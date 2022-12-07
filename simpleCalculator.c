47498d6a1f6f04c9560c1de4c1d57ce621f71fbd
YoochanJeong <saycruder123@gmail.com>
2022-12-08 01:24:47 +0900

    multiply 구현 완료

--- simpleCalculator.c
+++ simpleCalculator.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+int multiply(int x, int y){
+	printf("%d * %d = %d", x, y, x * y);	
+}
+
 int plus(int x, int y){
-	printf("%d + %d = %d", x, y, x + y);
+	printf("%d + %d = %d\n", x, y, x + y);
 }
 
 
@@ -11,6 +15,7 @@ int main()
 	printf("두 수 입력: ");
 	scanf("%d %d", &x, &y);	
 	plus(x, y);
+	multiply(x, y);
 	
 	return 0;	
 }
