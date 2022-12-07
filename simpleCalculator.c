7e58ba3f9c71575b428d5bcf67dc4caa3f9ff1bc
YoochanJeong <saycruder123@gmail.com>
2022-12-08 00:32:08 +0900

    plus 구현 완료

--- simpleCalculator.c
+++ simpleCalculator.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
+int plus(int x, int y){
+	printf("%d + %d = %d", x, y, x + y);
+}
+
 
 int main()
 {
-	printf("main 구현 완료");
+	int x, y;
+	printf("두 수 입력: ");
+	scanf("%d %d", &x, &y);	
+	plus(x, y);
 	
 	return 0;	
 }
